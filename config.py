507fc2ea00694fd77cb810ee96863568b85c067b
sam <shea093@aucklanduni.ac.nz>
2023-08-23 18:05:57 +1200

    need to amend to make a Top 4 work (currently not enough people in LOW GROUP to make a bottom 2)

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ RANDOM_SEED = 5545  # Set the random seed for reproducibility
 RANDOM_VARIABILITY = 0.1 # Set the percentage of random variability for episode scores
 DB_COLUMNS = ['name', 'charisma', 'creativity', 'runway_presence', 'performance_skills', 'sewing_and_crafting',
                'comedy', 'drama_and_emotion', 'adaptability', 'teamwork', 'confidence', 'lipsyncing']
+#Variable for setting % of a double win chance, use 1-100
+DOUBLE_WINNER_CHANCE = 15
